7e27acac200d242489dbb7c1cc15fb60499a0f1f
batukaraca <35922329+batukaraca@users.noreply.github.com>
2018-11-30 01:27:56 +0300

    ilk

---
+++ WebProjesi/Controllers/WordController.cs
@@ -0,0 +1,35 @@
+﻿using Proje.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Proje.Controllers
+{
+    public class WordController : Controller
+    {
+        // domain/Word/List 
+
+        public ActionResult List()
+        {
+            List<Word> Word = new List<Word>();
+            Deserialize(Word);
+            return View(Word);
+        }
+        static void Deserialize(List<Word> Deneme)
+        {
+            FileStream fs = new FileStream(@"C:\Users\Toshiba\source\repos\ConsoleApp1\ConsoleApp1\bin\Debug\Word.xml", FileMode.Open);
+            XmlSerializer xs = new XmlSerializer(typeof(List<Word>));
+            xs.Deserialize(fs);
+            fs.Close();
+        }
+
+
+    }
+}
---
+++ WebProjesi/Models/Word.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Xml.Serialization;
+
+namespace Proje.Models
+{
+
+
+    public class Word
+    {
+
+        [XmlAttribute(AttributeName = "text")]
+
+        public string Words { get; set; }
+
+        [XmlAttribute(AttributeName = "count")]
+
+        public int Count { get; set; }
+    }
+}
